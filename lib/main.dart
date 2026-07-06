312fcf1d26f351d51ebde2b80654b1280a38c114
MUHAMMAD SHAFAN RIAZ <malikshafan80@gmail.com>
2026-07-06 14:22:55 +0500

    chore: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Smart Resume Builder',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
